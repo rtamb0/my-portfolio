e713024743e0f2de298db5e4b616e0bc1d3015cc
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-07-28 14:01:47 +0700

    Make the layout mobile responsive

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@ import Topbar from './components/TopBar.vue'
 </script>
 
 <template>
-  <div id="app-layout">
-    <Sidebar />
+  <div id="app-layout" class="flex h-screen w-screen">
+    <Sidebar id="sidebar" />
     <div id="app-content">
       <Topbar />
       <RouterView />
@@ -13,4 +13,22 @@ import Topbar from './components/TopBar.vue'
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Mobile Devices (Smartphones up to 480px wide) */
+@media screen and (max-width: 768px) {
+  #app-layout {
+    flex-direction: column;
+    max-width: 100%;
+  }
+}
+
+/* Laptops and Desktops (Widths 769px and above) */
+@media screen and (min-width: 769px) {
+  #sidebar {
+    width: 12.5%;
+  }
+  #app-content {
+    width: 87.5%;
+  }
+}
+</style>
