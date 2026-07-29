1c139c56f5f9d56ca9a2fd41c3081446972ad19e
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-07-29 15:13:11 +0700

    Create hover and selected styling for nav buttons

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -12,7 +12,7 @@ const menus = ref(menuStore.getMenuList)
       <li
         v-for="menu in menus"
         :key="menu.name"
-        class="h-16 w-16 rotate-45 button-color button-border"
+        class="button h-16 w-16 rotate-45 button-color button-border"
       >
         <RouterLink :to="menu.link" class="flex h-full w-full items-center justify-center">
           <span class="block whitespace-nowrap button-text text-white -rotate-55">
@@ -49,6 +49,38 @@ const menus = ref(menuStore.getMenuList)
     rgb(102, 154, 237) 0.567324px -1.91785px 0px,
     rgb(102, 154, 237) 1.41734px -1.41108px 0px,
     rgb(102, 154, 237) 1.92034px -0.558831px 0px;
+
+  transition:
+    color 0.2s ease-in-out,
+    text-shadow 0.2s ease-in-out;
+}
+
+.router-link-active .button-text {
+  color: rgb(255, 199, 244);
+  text-shadow:
+    rgb(235, 105, 210) 2px 0 0,
+    rgb(235, 105, 210) 1.75517px 0.958851px 0,
+    rgb(235, 105, 210) 1.0806px 1.68294px 0,
+    rgb(235, 105, 210) 0.141474px 1.99499px 0,
+    rgb(235, 105, 210) -0.832294px 1.81859px 0,
+    rgb(235, 105, 210) -1.60229px 1.19694px 0,
+    rgb(235, 105, 210) -1.97998px 0.28224px 0,
+    rgb(235, 105, 210) -1.87291px -0.701566px 0,
+    rgb(235, 105, 210) -1.30729px -1.5136px 0,
+    rgb(235, 105, 210) -0.421592px -1.95506px 0,
+    rgb(235, 105, 210) 0.567324px -1.91785px 0,
+    rgb(235, 105, 210) 1.41734px -1.41108px 0,
+    rgb(235, 105, 210) 1.92034px -0.558831px 0,
+    #ff2d95 0px 0px 20px,
+    #ff2d95 0px 0px 30px,
+    #ff2d95 0px 0px 40px,
+    #ff2d95 0px 0px 50px,
+    #ff2d95 0px 0px 75px,
+    -10px -22px 9px rgba(235, 105, 210, 0);
+
+  transition:
+    color 0.2s ease-in-out,
+    text-shadow 0.2s ease-in-out;
 }
 
 .button-border {
@@ -61,5 +93,14 @@ const menus = ref(menuStore.getMenuList)
 
 .button-wrapper {
   background-color: rgba(84, 178, 242, 0.7);
+}
+
+.button:hover {
+  transform: scale(1.1);
+  transition: transform 0.2s ease-in-out;
+}
+
+.button {
+  transition: transform 0.2s ease-in-out;
 }
 </style>
