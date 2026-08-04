f2a2d07341ac91b7ad51ceae53aa2e0e108dce9e
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-04 19:35:33 +0700

    Create background info card

--- src/views/AboutRoute.vue
+++ src/views/AboutRoute.vue
@@ -1,1 +1,37 @@
-<template></template>
+<template>
+  <div class="flex flex-col p-10 items-center">
+    <!-- Job-title banner -->
+    <div class="flex relative h-10">
+      <!-- Left edge -->
+      <div
+        class="relative border-r-20 border-t-20 border-t-transparent border-b-20 border-b-transparent border-r-[#4a67d6]"
+      ></div>
+      <!-- Job-title body -->
+      <div class="flex h-10 px-10 items-center overflow-hidden bg-[#4a67d6]">
+        <h3 class="font-semibold italic text-white text-xl">Background</h3>
+      </div>
+      <!-- Right edge -->
+      <div
+        class="relative border-l-20 border-t-20 border-t-transparent border-b-20 border-b-transparent border-l-[#4a67d6]"
+      ></div>
+    </div>
+    <div class="bg-[#48438a]/80 -mt-5 pt-10 px-5 pb-5">
+      <div class="bg-[#31409e] p-5 text-white">
+        <p>
+          I'm Ralf, a current Computer Science student who enjoys creating websites that feel clean, responsive,
+          and easy to use. I like combining design and development to build interfaces that are both
+          visually appealing and practical.
+          <pre> </pre>
+          Most of my experience comes from working with Vue.js and modern front-end technologies,
+          where I've developed features, integrated REST APIs, and translated designs into
+          functional web applications. I'm constantly learning, experimenting with new tools, and
+          looking for ways to improve both my technical skills and the user experience of the
+          products I build.
+          <pre> </pre>
+          Outside of coding, I enjoy exploring UI design, studying how great products solve user
+          problems, and challenging myself with new projects that help me grow as a developer.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
