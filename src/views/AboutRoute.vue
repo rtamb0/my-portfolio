5811b0a4d0724880c71a2fe1cd3414bc53db6ae7
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-05 11:23:17 +0700

    Fix the html formatting for background card content

--- src/views/AboutRoute.vue
+++ src/views/AboutRoute.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="p-10 h-full grid grid-cols-2">
-    <WebsiteCard
-      class="row-span-2"
-      title="Background"
-    >
+    <WebsiteCard class="row-span-2" title="Background">
       <p>
-        Hey there! I'm Ralf, a current Computer Science student that specialises in web development, particularly front-end development.
-        I enjoy creating websites that feel clean, responsive,
-        and also combining design and development to build interfaces that are both
-        visually appealing and practical.
-        <pre> </pre>
+        Hey there! I'm Ralf, a current Computer Science student that specialises in web development,
+        particularly front-end development. I enjoy creating websites that feel clean, responsive,
+        and also combining design and development to build interfaces that are both visually
+appealing and practical.
+        </p>
+      <br />
+      <p>
         Most of my experience comes from working with Vue.js and modern front-end technologies,
-        where I've developed features, integrated REST APIs, and translated designs and requirements into
-        functional web applications. I'm constantly learning, experimenting with new tools, and
-        looking for ways to improve both my technical skills and the user experience of the
-        products I build.
-        <pre> </pre>
-        Other than web development, I am also interested in UI/UX design, and I enjoy exploring the intersection of design and development to create
-        interfaces that are both visually appealing and practical.
-        <pre> </pre>
-        In my portfolio, you'll find a collection of my projects that showcase my skills and experience in web development.
+        where I've developed features, integrated REST APIs, and translated designs and requirements
+        into functional web applications. I'm constantly learning, experimenting with new tools, and
+        looking for ways to improve both my technical skills and the user experience of the products
+I build.
+        </p>
+      <br />
+      <p>
+        Other than web development, I am also interested in UI/UX design, and I enjoy exploring the
+        intersection of design and development to create interfaces that are both visually appealing
+and practical.
+        </p>
+      <br />
+      <p>
+        In my portfolio, you'll find a collection of my projects that showcase my skills and
+experience in web development.
       </p>
     </WebsiteCard>
   </div>
