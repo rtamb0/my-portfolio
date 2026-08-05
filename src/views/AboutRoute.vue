2f638874a4f3cefb40e4fc09a0ab24e342adc5c4
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-05 11:43:05 +0700

    Add prettier formatting

--- src/views/AboutRoute.vue
+++ src/views/AboutRoute.vue
@@ -5,26 +5,26 @@
         Hey there! I'm Ralf, a current Computer Science student that specialises in web development,
         particularly front-end development. I enjoy creating websites that feel clean, responsive,
         and also combining design and development to build interfaces that are both visually
-appealing and practical.
-        </p>
+        appealing and practical.
+      </p>
       <br />
       <p>
         Most of my experience comes from working with Vue.js and modern front-end technologies,
         where I've developed features, integrated REST APIs, and translated designs and requirements
         into functional web applications. I'm constantly learning, experimenting with new tools, and
         looking for ways to improve both my technical skills and the user experience of the products
-I build.
-        </p>
+        I build.
+      </p>
       <br />
       <p>
         Other than web development, I am also interested in UI/UX design, and I enjoy exploring the
         intersection of design and development to create interfaces that are both visually appealing
-and practical.
-        </p>
+        and practical.
+      </p>
       <br />
       <p>
         In my portfolio, you'll find a collection of my projects that showcase my skills and
-experience in web development.
+        experience in web development.
       </p>
     </WebsiteCard>
   </div>
