db86eb8e439dc4a9fed062e5f3c906239edcb85b
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-06 15:39:39 +0700

    Change router history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: () => import('../views/HomeRoute.vue') },
@@ -8,7 +8,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: routes,
 })
 
