f3cbe2214be2505f8a42e9a0db9d976e6425c2fa
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-06 12:53:52 +0700

    Add working link for resume/cv

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -4,7 +4,13 @@
       <p class="text-sm">&copy; 2026 Ralf Fadilla Tambayong. All rights reserved.</p>
       <p class="text-sm ml-4">
         Want to download my Resume/CV?
-        <a href="/resume.pdf" download class="underline">Click here</a>
+        <a
+          href="https://1drv.ms/b/c/e440bf5b26b439ba/IQB0vJYwdINFR4hkUO5cbsDQAR4HTRR_7hquud1vfKQaI7A?e=wfu0y7"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="underline"
+          >Click here</a
+        >
       </p>
     </div>
   </div>
