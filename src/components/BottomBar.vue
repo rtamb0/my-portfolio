b0e0ca16400f3e192ed41a744248cb10973c8130
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-08 14:57:52 +0700

    Make bottom bar mobile responsive

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-[#3e5bc3]/80 text-white h-8 flex items-center px-3">
+  <div class="bg-[#3e5bc3]/80 text-white h-full md:h-8 flex items-center px-3">
     <div class="flex items-center justify-between w-full">
       <p class="text-sm">&copy; 2026 Ralf Fadilla Tambayong. All rights reserved.</p>
       <p class="text-sm ml-4">
