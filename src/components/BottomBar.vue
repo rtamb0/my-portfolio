ef7d7849c31520970217d35188d7c667549101af
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-10 10:26:59 +0700

    Update resume link

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -4,11 +4,7 @@
       <p class="text-sm">&copy; 2026 Ralf Fadilla Tambayong. All rights reserved.</p>
       <p class="text-sm ml-4">
         Want to download my Resume/CV?
-        <a
-          href="https://1drv.ms/b/c/e440bf5b26b439ba/IQB0vJYwdINFR4hkUO5cbsDQAR4HTRR_7hquud1vfKQaI7A?e=wfu0y7"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="underline"
+        <a href="https://bit.ly/3TMiJ6S" target="_blank" rel="noopener noreferrer" class="underline"
           >Click here</a
         >
       </p>
