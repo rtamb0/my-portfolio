cb504b0706c35def03a228046db25ffa3f71fd02
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-10 11:29:37 +0700

    Change bottom bar from my name to my github username

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="bg-[#3e5bc3]/80 text-white h-full md:h-8 flex items-center px-3">
     <div class="flex items-center justify-between w-full">
-      <p class="text-sm">&copy; 2026 Ralf Fadilla Tambayong. All rights reserved.</p>
+      <p class="text-sm">
+        &copy; 2026
+        <a
+          href="https://github.com/rtamb0"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="underline"
+          >rtamb0</a
+        >. All rights reserved.
+      </p>
       <p class="text-sm ml-4">
         Want to download my Resume/CV?
         <a href="https://bit.ly/3TMiJ6S" class="underline">Click here</a>
